519419506af90f58da3a8a8407e3e465bef84129
Olaf <olaf@privacymachine.eu>
2016-07-21 01:46:13 +0200

    prepare for storing open time record

--- monitor_big_switch.py
+++ monitor_big_switch.py
@@ -9,8 +9,16 @@ This script runs on a RaspberryPi, monitors the swicht and triggers SpaceAPI cha
 
 from __future__ import print_function
 from time import sleep
+import arrow
 import subprocess as sp
 import RPi.GPIO as GPIO
+
+
+# print to STDERR
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
 
 #===================== Exexute a shell command ====================================
 def cmd_exec(cmd,args):
@@ -21,7 +29,7 @@ def cmd_exec(cmd,args):
     try:
         str1 = sp.check_output([cmd]+args, stderr=sp.STDOUT)
     except sp.CalledProcessError as err:
-        print('ERROR: Execution failed\n cmd: '+str(err.cmd)+'\n output: '+str(err.output)+'\n retunrcode: '+ str(err.returncode))
+        eprint('ERROR: Execution failed\n cmd: '+str(err.cmd)+'\n output: '+str(err.output)+'\n retunrcode: '+ str(err.returncode))
         return err.returncode
 #    print('SUCCESS: executed '+cmd_str)
 
@@ -36,29 +44,49 @@ GPIO.output(3, GPIO.LOW)
 
 state = GPIO.input(2)
 error = False
+first_loop = True
+
+time_var = arrow.Arrow.now()
+
+# TODO: read from https://heidelberg-makerspace/status.json
+longest_open = 0
 
 while True:
+    # In case of error blick with red LED
     if error:
         GPIO.output(3, GPIO.LOW)
-        sleep(1)
+        sleep(.5)
         GPIO.output(3, GPIO.HIGH)
-        sleep(1)
+        sleep(.5)
         continue
         
     
     
     sleep(1)
     new_state = GPIO.input(2)
-    if state != new_state:
+    
+    if state != new_state or first_loop:
         print("set state to: "+ str(GPIO.input(2)))
         state = new_state
-        if state == 1:
+        if state == 0:
+            # change state to "open"
             cmd_exec('ssh',['-i','/root/.ssh/makerspace_hb_open',
                             'spaceapi@heidelberg-makerspace.de'])
-        elif state ==0: 
+            # start time messurement
+            time_var = arrow.Arrow.now()
+                            
+        elif state ==1: 
+            # calculate current presence time
+            tmp = arrow.Arrow.now() - time_var            
+            tmp = tmp.days*24*60*60 + tmp.secounds
+            # check for new precentce time record
+            if tmp > longest_open:
+                longest_open = tmp
+            #TODO: cange state to closed and reset precentce time record
             cmd_exec('ssh',['-i','/root/.ssh/makerspace_hb_close',
                             'spaceapi@heidelberg-makerspace.de'])
         else:
-            print('ERROR: Invalid state '+str(state)+'\nValid states are "0" and "1"')
+            eprint('ERROR: Invalid state '+str(state)+'\nValid states are "0" and "1"')
             error = True
-        
+    
+    first_loop = False
